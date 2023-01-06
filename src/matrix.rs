19b578b09d0345c040d3ba0be517ecacb25fea6c
simoncarlson <cimon.carlson@gmail.com>
2023-01-06 13:34:04 +0100

    Matrix index operator/function

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,7 @@
 use crate::Vector3D;
 
+use std::ops::Index;
+
 pub struct Matrix3D {
     n: [[f64; 3]; 3],
 }
@@ -15,6 +17,18 @@ impl Matrix3D {
     pub fn from_vector(a: Vector3D, b: Vector3D, c: Vector3D) -> Self {
         let n = [[a.x, a.y, a.z], [b.x, b.y, b.z], [c.x, c.y, c.z]];
         Self { n }
+    }
+
+    pub fn vector(&self, j: usize) -> Vector3D {
+        let [x, y, z] = self.n[j];
+        Vector3D { x, y, z }
+    }
+}
+
+impl Index<[usize; 2]> for Matrix3D {
+    type Output = f64;
+    fn index(&self, index: [usize; 2]) -> &Self::Output {
+       &self.n[index[1]][index[0]]
     }
 }
 
@@ -51,6 +65,37 @@ mod tests {
         assert_eq!(matrix.n[2][0], 0.7);
         assert_eq!(matrix.n[2][1], 0.8);
         assert_eq!(matrix.n[2][2], 0.9);
+    }
+
+    #[test]
+    fn index() {
+        let matrix = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        assert_eq!(matrix[[0,0]], 0.1);
+        assert_eq!(matrix[[0,1]], 0.2);
+        assert_eq!(matrix[[0,2]], 0.3);
+        assert_eq!(matrix[[1,0]], 0.4);
+        assert_eq!(matrix[[1,1]], 0.5);
+        assert_eq!(matrix[[1,2]], 0.6);
+        assert_eq!(matrix[[2,0]], 0.7);
+        assert_eq!(matrix[[2,1]], 0.8);
+        assert_eq!(matrix[[2,2]], 0.9);
+    }
+
+    #[test]
+    fn vector_index() {
+        let vector1 = Vector3D::new(0.1, 0.2, 0.3);
+        let vector2 = Vector3D::new(0.4, 0.5, 0.6);
+        let vector3 = Vector3D::new(0.7, 0.8, 0.9);
+        let matrix = Matrix3D::from_vector(vector1, vector2, vector3);
+        assert_eq!(matrix.vector(0)[0], 0.1);
+        assert_eq!(matrix.vector(0)[1], 0.2);
+        assert_eq!(matrix.vector(0)[2], 0.3);
+        assert_eq!(matrix.vector(1)[0], 0.4);
+        assert_eq!(matrix.vector(1)[1], 0.5);
+        assert_eq!(matrix.vector(1)[2], 0.6);
+        assert_eq!(matrix.vector(2)[0], 0.7);
+        assert_eq!(matrix.vector(2)[1], 0.8);
+        assert_eq!(matrix.vector(2)[2], 0.9);
     }
 
 }
