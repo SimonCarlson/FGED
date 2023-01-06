a5b1e7b348210d432808729ac40e6818bd57d2a9
simoncarlson <cimon.carlson@gmail.com>
2023-01-06 12:39:01 +0100

    3d matrix sotred column-major mode

---
+++ src/matrix.rs
@@ -0,0 +1,56 @@
+use crate::Vector3D;
+
+pub struct Matrix3D {
+    n: [[f64; 3]; 3],
+}
+
+impl Matrix3D {
+    pub fn new(n00: f64, n01: f64, n02: f64,
+        n10: f64, n11: f64, n12: f64,
+        n20: f64, n21: f64, n22: f64) -> Self {
+            let n = [[n00, n10, n20], [n01, n11, n21], [n02, n12, n22]];
+            Self { n }
+    }
+
+    pub fn from_vector(a: Vector3D, b: Vector3D, c: Vector3D) -> Self {
+        let n = [[a.x, a.y, a.z], [b.x, b.y, b.z], [c.x, c.y, c.z]];
+        Self { n }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructor() {
+        let matrix = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        assert_eq!(matrix.n[0][0], 0.1);
+        assert_eq!(matrix.n[0][1], 0.4);
+        assert_eq!(matrix.n[0][2], 0.7);
+        assert_eq!(matrix.n[1][0], 0.2);
+        assert_eq!(matrix.n[1][1], 0.5);
+        assert_eq!(matrix.n[1][2], 0.8);
+        assert_eq!(matrix.n[2][0], 0.3);
+        assert_eq!(matrix.n[2][1], 0.6);
+        assert_eq!(matrix.n[2][2], 0.9);
+    }
+
+    #[test]
+    fn vector_constructor() {
+        let vector1 = Vector3D::new(0.1, 0.2, 0.3);
+        let vector2 = Vector3D::new(0.4, 0.5, 0.6);
+        let vector3 = Vector3D::new(0.7, 0.8, 0.9);
+        let matrix = Matrix3D::from_vector(vector1, vector2, vector3);
+        assert_eq!(matrix.n[0][0], 0.1);
+        assert_eq!(matrix.n[0][1], 0.2);
+        assert_eq!(matrix.n[0][2], 0.3);
+        assert_eq!(matrix.n[1][0], 0.4);
+        assert_eq!(matrix.n[1][1], 0.5);
+        assert_eq!(matrix.n[1][2], 0.6);
+        assert_eq!(matrix.n[2][0], 0.7);
+        assert_eq!(matrix.n[2][1], 0.8);
+        assert_eq!(matrix.n[2][2], 0.9);
+    }
+
+}
