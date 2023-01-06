6154b50f2a87b9ef0a7bb0bab85e78cad9a7b61c
simoncarlson <cimon.carlson@gmail.com>
2023-01-06 17:25:49 +0100

    Matrix addition, subtraction, and scalar multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,7 +1,8 @@
 use crate::Vector3D;
 
-use std::ops::Index;
+use std::ops::{Add, Index, Mul, Sub};
 
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Matrix3D {
     n: [[f64; 3]; 3],
 }
@@ -25,16 +26,57 @@ impl Matrix3D {
     }
 }
 
+impl Add<Self> for Matrix3D {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+       Matrix3D::new(self[[0,0]] + rhs[[0,0]], self[[0,1]] + rhs[[0,1]], self[[0,2]] + rhs[[0,2]],
+        self[[1,0]] + rhs[[1,0]], self[[1,1]] + rhs[[1,1]], self[[1,2]] + rhs[[1,2]],
+        self[[2,0]] + rhs[[2,0]], self[[2,1]] + rhs[[2,1]], self[[2,2]] + rhs[[2,2]])
+    }
+}
+
 impl Index<[usize; 2]> for Matrix3D {
     type Output = f64;
     fn index(&self, index: [usize; 2]) -> &Self::Output {
-       &self.n[index[1]][index[0]]
+        if index[0] > 2 {
+            panic!("Index {} out of range", index[0]);
+        }
+        if index[1] > 2 {
+            panic!("Index {} out of range", index[1]);
+        }
+        &self.n[index[1]][index[0]]
+    }
+}
+
+impl Mul<f64> for Matrix3D {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Matrix3D::new(self[[0,0]] * rhs, self[[0,1]] * rhs, self[[0,2]] * rhs,
+        self[[1,0]] * rhs, self[[1,1]] * rhs, self[[1,2]] * rhs,
+        self[[2,0]] * rhs, self[[2,1]] * rhs, self[[2,2]] * rhs)
+    }
+}
+
+impl Mul<Matrix3D> for f64 {
+    type Output = Matrix3D;
+    fn mul(self, rhs: Matrix3D) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl Sub<Self> for Matrix3D {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+       Matrix3D::new(self[[0,0]] - rhs[[0,0]], self[[0,1]] - rhs[[0,1]], self[[0,2]] - rhs[[0,2]],
+        self[[1,0]] - rhs[[1,0]], self[[1,1]] - rhs[[1,1]], self[[1,2]] - rhs[[1,2]],
+        self[[2,0]] - rhs[[2,0]], self[[2,1]] - rhs[[2,1]], self[[2,2]] - rhs[[2,2]])
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_approx_eq::assert_approx_eq;
 
     #[test]
     fn constructor() {
@@ -96,6 +138,53 @@ mod tests {
         assert_eq!(matrix.vector(2)[0], 0.7);
         assert_eq!(matrix.vector(2)[1], 0.8);
         assert_eq!(matrix.vector(2)[2], 0.9);
+    }
+
+    #[test]
+    fn matrix_addition() {
+        let matrix1 = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        let matrix2 = matrix1 + matrix1;
+        assert_approx_eq!(matrix2[[0,0]], 0.2);
+        assert_approx_eq!(matrix2[[0,1]], 0.4);
+        assert_approx_eq!(matrix2[[0,2]], 0.6);
+        assert_approx_eq!(matrix2[[1,0]], 0.8);
+        assert_approx_eq!(matrix2[[1,1]], 1.0);
+        assert_approx_eq!(matrix2[[1,2]], 1.2);
+        assert_approx_eq!(matrix2[[2,0]], 1.4);
+        assert_approx_eq!(matrix2[[2,1]], 1.6);
+        assert_approx_eq!(matrix2[[2,2]], 1.8);
+    }
+
+    #[test]
+    fn matrix_subtraction() {
+        let matrix1 = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        let matrix2 = Matrix3D::new(0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8);
+        let matrix3 = matrix2 - matrix1;
+        assert_approx_eq!(matrix3[[0,0]], 0.1);
+        assert_approx_eq!(matrix3[[0,1]], 0.2);
+        assert_approx_eq!(matrix3[[0,2]], 0.3);
+        assert_approx_eq!(matrix3[[1,0]], 0.4);
+        assert_approx_eq!(matrix3[[1,1]], 0.5);
+        assert_approx_eq!(matrix3[[1,2]], 0.6);
+        assert_approx_eq!(matrix3[[2,0]], 0.7);
+        assert_approx_eq!(matrix3[[2,1]], 0.8);
+        assert_approx_eq!(matrix3[[2,2]], 0.9);
+    }
+
+    #[test]
+    fn scalar_multiplication() {
+        let matrix1 = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        let matrix2 = matrix1 * 5.0;
+        assert_approx_eq!(matrix2[[0,0]], 0.5);
+        assert_approx_eq!(matrix2[[0,1]], 1.0);
+        assert_approx_eq!(matrix2[[0,2]], 1.5);
+        assert_approx_eq!(matrix2[[1,0]], 2.0);
+        assert_approx_eq!(matrix2[[1,1]], 2.5);
+        assert_approx_eq!(matrix2[[1,2]], 3.0);
+        assert_approx_eq!(matrix2[[2,0]], 3.5);
+        assert_approx_eq!(matrix2[[2,1]], 4.0);
+        assert_approx_eq!(matrix2[[2,2]], 4.5);
+        assert_eq!(matrix2, 5.0 * matrix1);
     }
 
 }
