4b3a85ff0863fc1dbdf9868705823713b4b0b467
simoncarlson <cimon.carlson@gmail.com>
2023-01-07 10:17:25 +0100

    Matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -57,6 +57,31 @@ impl Mul<f64> for Matrix3D {
     }
 }
 
+impl Mul<Matrix3D> for Matrix3D {
+    type Output = Self;
+    fn mul(self, rhs: Matrix3D) -> Self::Output {
+        Matrix3D::new(
+            self[[0,0]] * rhs[[0,0]] + self[[0,1]] * rhs[[1,0]] + self[[0,2]] * rhs[[2,0]],
+            self[[0,0]] * rhs[[0,1]] + self[[0,1]] * rhs[[1,1]] + self[[0,2]] * rhs[[2,1]],
+            self[[0,0]] * rhs[[0,2]] + self[[0,1]] * rhs[[1,2]] + self[[0,2]] * rhs[[2,2]],
+            self[[1,0]] * rhs[[0,0]] + self[[1,1]] * rhs[[1,0]] + self[[1,2]] * rhs[[2,0]],
+            self[[1,0]] * rhs[[0,1]] + self[[1,1]] * rhs[[1,1]] + self[[1,2]] * rhs[[2,1]],
+            self[[1,0]] * rhs[[0,2]] + self[[1,1]] * rhs[[1,2]] + self[[1,2]] * rhs[[2,2]],
+            self[[2,0]] * rhs[[0,0]] + self[[2,1]] * rhs[[1,0]] + self[[2,2]] * rhs[[2,0]],
+            self[[2,0]] * rhs[[0,1]] + self[[2,1]] * rhs[[1,1]] + self[[2,2]] * rhs[[2,1]],
+            self[[2,0]] * rhs[[0,2]] + self[[2,1]] * rhs[[1,2]] + self[[2,2]] * rhs[[2,2]])
+    }
+}
+
+impl Mul<Vector3D> for Matrix3D {
+    type Output = Vector3D;
+    fn mul(self, rhs: Vector3D) -> Self::Output {
+        Vector3D::new(self[[0,0]] * rhs.x + self[[0,1]] * rhs.y + self[[0,2]] * rhs.z,
+            self[[1,0]] * rhs.x + self[[1,1]] * rhs.y + self[[1,2]] * rhs.z,
+            self[[2,0]] * rhs.x + self[[2,1]] * rhs.y + self[[2,2]] * rhs.z) 
+    }
+}
+
 impl Mul<Matrix3D> for f64 {
     type Output = Matrix3D;
     fn mul(self, rhs: Matrix3D) -> Self::Output {
@@ -187,4 +212,30 @@ mod tests {
         assert_eq!(matrix2, 5.0 * matrix1);
     }
 
+    #[test]
+    fn matrix_multiplication() {
+        let matrix1 = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        let matrix2 = Matrix3D::new(0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8);
+        let matrix3 = matrix1 * matrix2;
+        let expected = Matrix3D::new(0.6, 0.72, 0.84, 1.32, 1.62, 1.92, 2.04, 2.52, 3.0);
+        assert_approx_eq!(matrix3[[0,0]], expected[[0,0]]);
+        assert_approx_eq!(matrix3[[0,1]], expected[[0,1]]);
+        assert_approx_eq!(matrix3[[0,2]], expected[[0,2]]);
+        assert_approx_eq!(matrix3[[1,0]], expected[[1,0]]);
+        assert_approx_eq!(matrix3[[1,1]], expected[[1,1]]);
+        assert_approx_eq!(matrix3[[1,2]], expected[[1,2]]);
+        assert_approx_eq!(matrix3[[2,0]], expected[[2,0]]);
+        assert_approx_eq!(matrix3[[2,1]], expected[[2,1]]);
+        assert_approx_eq!(matrix3[[2,2]], expected[[2,2]]);
+    }
+
+    #[test]
+    fn matrix_vector_multiplication() {
+        let matrix = Matrix3D::new(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
+        let vector1 = Vector3D::new(0.2, 0.4, 0.6);
+        let vector2 = matrix * vector1;
+        assert_approx_eq!(vector2[0], 0.1*0.2 + 0.2*0.4 + 0.3*0.6);
+        assert_approx_eq!(vector2[1], 0.4*0.2 + 0.5*0.4 + 0.6*0.6);
+        assert_approx_eq!(vector2[2], 0.7*0.2 + 0.8*0.4 + 0.9*0.6);
+    }
 }
