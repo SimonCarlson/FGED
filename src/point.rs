ad791b65f56bc052982e40df9c8fbb58dc60ec5d
simoncarlson <cimon.carlson@gmail.com>
2023-02-05 12:47:30 +0100

    Function for calculating distance from point to line and from line to line

--- src/point.rs
+++ src/point.rs
@@ -9,6 +9,33 @@ pub struct Point3D {
     pub z: f64,
 }
 
+pub fn distance_point_line(q: Point3D, p: Point3D, v: Vector3D) -> f64 {
+    let a = (q - p).cross(&v);
+    (a.dot(&a) / v.dot(&v)).sqrt()
+}
+
+pub fn distance_line_line(p1: Point3D, v1: Vector3D, p2: Point3D, v2: Vector3D) -> f64 {
+    let dp = p2 - p1;
+
+    let v12 = v1.dot(&v1);
+    let v22 = v2.dot(&v2);
+    let v1v2 = v1.dot(&v2);
+
+    let det = v1v2 * v1v2 - v12 * v22;
+    if det.abs() > f64::EPSILON {
+        let inv_det = 1.0 / det;
+        let dpv1 = dp.dot(&v1);
+        let dpv2 = dp.dot(&v2);
+        let t1 = (v1v2 * dpv2 - v22 * dpv1) * inv_det;
+        let t2 = (v12 * dpv2 - v1v2 * dpv1) * inv_det;
+
+        (dp + v2 * t2 - v1 * t1).magnitude()
+    } else {
+        let a = dp.cross(&v1);
+        (a.dot(&a) / v12).sqrt()
+    }
+}
+
 impl Point3D {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
